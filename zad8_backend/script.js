3b150434701dfc8bc4dd65e4e1afe5c980009565
PL75205 <kubakon05@gmail.com>
2026-05-11 11:11:44 +0200

    75205_zad8_ukonczone

--- zad8_backend/script.js
+++ zad8_backend/script.js
@@ -1,37 +1,24 @@
 // Import the functions you need from the SDKs you need
-// Firebase App
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firestore
-import {
-  getFirestore,
-  collection,
-  addDoc
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBuFnTrcwDcO469--gGDgWeOLlXFSuIiSg",
+    authDomain: "zad8-backend.firebaseapp.com",
+    projectId: "zad8-backend",
+    storageBucket: "zad8-backend.firebasestorage.app",
+    messagingSenderId: "467404249520",
+    appId: "1:467404249520:web:3c13624ae68393ec6e1e75",
+    measurementId: "G-H32QBQ3JTT"
+  };
 
-// Import Firestore functions
-import { getFirestore, collection, addDoc } from "firebase/firestore"; //
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBuFnTrcwDcO469--gGDgWeOLlXFSuIiSg",
-  authDomain: "zad8-backend.firebaseapp.com",
-  projectId: "zad8-backend",
-  storageBucket: "zad8-backend.firebasestorage.app",
-  messagingSenderId: "467404249520",
-  appId: "1:467404249520:web:684c8e029d18ae366e1e75",
-  measurementId: "G-7TMSWTC80H"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app); // Initialize Firestore database
-
-
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 // formularz
 const form = document.getElementById("form");
 const msg = document.getElementById("msg");
